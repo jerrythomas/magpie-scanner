efba9b823a8d7f491d6935cfe5cb32703418d30a
Jerry Thomas <me@jerrythomas.name>
2024-12-30 16:52:07 -0600

    chore: updated files and exports in packages

--- packages/patterns/src/index.js
+++ packages/patterns/src/index.js
@@ -1,6 +1,14 @@
+// skipcq: JS-E1004 - Needed for exposing all types
+export * from './types.js'
+// skipcq: JS-E1004 - Needed for exposing all common patterns
 export * from './common.js'
+// skipcq: JS-E1004 - Needed for exposing all file patterns
 export * from './files.js'
+// skipcq: JS-E1004 - Needed for exposing all path patterns
 export * from './path.js'
+// skipcq: JS-E1004 - Needed for exposing all show patterns
 export * from './shows.js'
+// skipcq: JS-E1004 - Needed for exposing all movie patterns
 export * from './movies.js'
+// skipcq: JS-E1004 - Needed for exposing all book patterns
 export * from './books.js'
